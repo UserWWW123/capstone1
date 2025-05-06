55ebe191f440cbc91460c4fba4ef97bb9d7f7561
KL <lamkayin1037@gmail.com>
2025-05-06 11:29:05 -0400

    1

--- js/dataTypeblocks.js
+++ js/dataTypeblocks.js
@@ -4,11 +4,7 @@ const draggable_list = document.getElementById('draggable-list');
     let codingSectionArray = [];
 
     const ifFunction = [
-      '1',
-      '2',
-      '3',
-      '4'
-      /*
+      
         'string',
         'string',
         'number',
@@ -18,7 +14,6 @@ const draggable_list = document.getElementById('draggable-list');
         'update',
         'door.img.offset.x = 10;',
         'door.img = "/assets/crate.png";'
-        */
         
       ];
       
